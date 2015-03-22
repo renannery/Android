cada295187df46fa8bc53cf20cd851cf517bd69a
Renan Nery <renannery@gmail.com>
2015-03-22 00:35:31 -0300

    Upload all project

---
+++ app/src/main/java/com/origamitecnologia/animations/MainActivity.java
@@ -0,0 +1,51 @@
+package com.origamitecnologia.animations;
+
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+
+
+public class MainActivity extends ActionBarActivity {
+    private Button btn1, btn2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn1 = (Button) findViewById(R.id.button);
+        btn2 = (Button) findViewById(R.id.button2);
+        ObjectAnimator oa = ObjectAnimator.ofFloat(btn2, "translationX", 0, 500);
+        ObjectAnimator oa2 = ObjectAnimator.ofFloat(btn1, "translationX", 0, -500);
+        AnimatorSet animatorSet = new AnimatorSet();
+        animatorSet.play(oa).with(oa2);
+        animatorSet.setDuration(1000);
+        animatorSet.start();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
